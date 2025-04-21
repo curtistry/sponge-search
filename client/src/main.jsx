c4008dbac1db98cf48c9b3980057440386ba9d7c
curtistry <138181400+curtistry@users.noreply.github.com>
2025-04-20 22:21:20 -0400

    fixed Vercel analytics

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Analytics } from "@vercel/analytics/react"
 import './index.css'
 import App from './App.jsx'
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Analytics>
+      <Analytics/>
       <App />
-    </Analytics>
   </StrictMode>,
 )
